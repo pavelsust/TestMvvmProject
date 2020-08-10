b51d1595df27128c8b7ebff29cf5a2cb77baf61a
paveltech <pavelcep.sust11@gmail.com>
2020-08-10 18:37:50 +0600

    json read done

--- app/src/main/java/com/fun/testmvvmproject/other/activity/DashboardActivity.kt
+++ app/src/main/java/com/fun/testmvvmproject/other/activity/DashboardActivity.kt
@@ -1,14 +1,10 @@
 package com.`fun`.testmvvmproject.other.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.FragmentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.`fun`.testmvvmproject.R
-import com.`fun`.testmvvmproject.repositry.DayMessageRepository
 import com.`fun`.testmvvmproject.utils.USER_EMAIL
 import com.`fun`.testmvvmproject.viewmodel.DayMessageViewModel
 import com.`fun`.testmvvmproject.viewmodel.ProblemViewModel
@@ -18,26 +14,36 @@ import kotlinx.android.synthetic.main.content_dashboard.*
 class DashboardActivity : AppCompatActivity() {
 
     var dayMessageViewModel: DayMessageViewModel? = null
-    var problemViewModel:ProblemViewModel?=null
+    var problemViewModel: ProblemViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         setSupportActionBar(toolbar)
 
-        //var intent = intent
-        //var emailAddress = intent.extras?.getString(USER_EMAIL)
-        //text_email.text = emailAddress
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        toolbar.setNavigationOnClickListener { finish() }
+
+
+        var intent = intent
+        var emailAddress = intent.extras?.getString(USER_EMAIL)
+        text_email.text = "User Email: $emailAddress"
 
         dayMessageViewModel = ViewModelProviders.of(this).get(DayMessageViewModel::class.java)
         dayMessageViewModel!!.getDayMessage().observe(this, Observer {
             text_geetings.text = it
         })
 
+        problemViewModel = ViewModelProviders.of(this).get(ProblemViewModel::class.java)
+        problemViewModel!!.getProblem().observe(this, Observer {
+            var item =
+                it.problems[0].diabetes[0].medications[0].medicationsClasses[0].className[0].associatedDrug[0]
 
-        problemViewModel = ViewModelProviders.of(this ).get(ProblemViewModel::class.java)
-        problemViewModel!!.getProblem().observe(this , Observer {
-            Log.d("SHOW" , ""+it.problems[0].diabetes[0].medications[0].medicationsClasses[0].className[0].associatedDrug[0].name)
+            text_drag.text =
+                "Name: ${item.name}\n" + "Dose: ${item.dose}\n" + "Strength: ${item.strength}"
+
         })
 
     }
